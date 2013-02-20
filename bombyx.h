ece3b2c7b51dd7cd484a7f7f555d1785c1570d63
lysandre corjon <corjon_l@new-user.(none)>
2013-02-20 19:46:10 +0100

    hum hum

---
+++ bombyx.h
@@ -0,0 +1,17 @@
+/*
+** bombyx.h for bombyx in /home/corjon_l//projets/All
+** 
+** Made by lysandre corjon
+** Login   <corjon_l@epitech.net>
+** 
+** Started on  Wed Feb 20 19:11:25 2013 lysandre corjon
+** Last update Wed Feb 20 19:15:57 2013 lysandre corjon
+*/
+
+#ifndef BOMBYX_H_
+# define BOMBYX_H_
+
+# define	SCREEN_H	(600)
+# define	SCREEN_W	(600)
+
+#endif /* !BOMBYX_H_ */
