29655f29af7b0eb7d93fbe18a329432785d0b4b9
lysandre corjon <corjon_l@new-user.(none)>
2013-02-20 19:30:17 +0100

    error.c up

--- error.c
+++ error.c
@@ -5,26 +5,14 @@
 ** Login   <richar_x@epitech.net>
 ** 
 ** Started on  Sat Jan 19 18:35:15 2013 theo richard
-** Last update Wed Feb 20 19:19:53 2013 theo richard
+** Last update Wed Feb 20 19:29:45 2013 lysandre corjon
 */
-#include <unistd.h>
-#include <stdlib.h>
-#include "allum1.h"
-#include "my.h"
 
-void	my_put_error(char c)
-{
-  write(2, &c, 1);
-}
+#include <string.h>
+#include <unistd.h>
 
 void	my_error(char *str)
 {
-  int	i;
-
-  i = 0;
-  while (str[i] != '\0')
-    my_put_error(str[i++]);
-  if (i > 0)
-    my_put_error('\n');
-  exit(42);
+  write(2, str, strlen(str));
+  return;
 }
