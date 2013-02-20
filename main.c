32c54991229b2b0c0aae11d35136f067aea3e35d
lysandre corjon <corjon_l@new-user.(none)>
2013-02-20 19:09:37 +0100

    up main

--- main.c
+++ main.c
@@ -5,5 +5,12 @@
 ** Login   <corjon_l@epitech.net>
 ** 
 ** Started on  Wed Feb 20 18:43:36 2013 lysandre corjon
-** Last update Wed Feb 20 18:43:37 2013 lysandre corjon
+** Last update Wed Feb 20 19:06:26 2013 lysandre corjon
 */
+
+int	main(int argc, char **argv, char **env)
+{
+  if (check_env(env) != 0)
+    return (-1);
+  return (0);
+}
