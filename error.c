377bed3db321c91e69e293928783ef7417ce1352
lysandre corjon <corjon_l@new-user.(none)>
2013-02-20 19:48:58 +0100

    error

--- error.c
+++ error.c
@@ -6,17 +6,10 @@
 ** 
 ** Started on  Sat Jan 19 18:35:15 2013 theo richard
 <<<<<<< HEAD
-** Last update Wed Feb 20 19:40:47 2013 theo richard
+** Last update Wed Feb 20 19:48:34 2013 lysandre corjon
 */
 #include <unistd.h>
 #include <stdlib.h>
-=======
-** Last update Wed Feb 20 19:36:56 2013 lysandre corjon
-*/
->>>>>>> c847af8a95b3ddb901515b9e5e08c3b314ba767c
-
-#include <string.h>
-#include <unistd.h>
 
 void	my_error(char *str)
 {
