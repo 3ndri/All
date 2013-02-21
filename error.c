691069c04ba880aa3b33af1222464c6a951ce118
theo richard <richar_x@pc-richar_x.(none)>
2013-02-21 18:09:20 +0100

    add my-error

--- error.c
+++ error.c
@@ -6,13 +6,16 @@
 ** 
 ** Started on  Sat Jan 19 18:35:15 2013 theo richard
 <<<<<<< HEAD
-** Last update Wed Feb 20 19:48:34 2013 lysandre corjon
+** Last update Wed Feb 20 19:51:53 2013 theo richard
 */
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 void	my_error(char *str)
 {
   write(2, str, strlen(str));
+  if (strlen(str) != 0)
+    my_putchar('\n');
   exit(42);
 }
